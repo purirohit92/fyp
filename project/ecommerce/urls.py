6e039660dc8e76b53dd6b91ac8b4d0ee5bbb575d
ganeshkuikel <ganeshkuikel66@gmail.com>
2023-01-07 12:15:58 +0545

    fix blogpost issue

--- project/ecommerce/urls.py
+++ project/ecommerce/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 urlpatterns = [
-    path('jet/', include('jet.urls','jet')),
+    # path('jet/', include('jet.urls','jet')),
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
